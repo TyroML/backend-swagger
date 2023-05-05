e4274fbfebe5693ed7bb979fa29c2d3d5d2ece1a
matined <nedzimateusz@gmail.com>
2023-05-05 18:52:45 +0200

    changes

---
+++ generate_swagger_file.py
@@ -0,0 +1,19 @@
+import os
+import sys
+import yaml
+
+from fastapi.openapi.utils import get_openapi
+
+sys.path.append(os.path.join(os.path.dirname(__file__), "backend/app/api"))
+
+from main import app
+
+
+with open('swagger.yaml', 'w') as f:
+    yaml.dump(get_openapi(
+        title=app.title,
+        version=app.version,
+        openapi_version=app.openapi_version,
+        description=app.description,
+        routes=app.routes
+    ), f)
