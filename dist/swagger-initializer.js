b17afcddc7f56dccaf7c58aaa4af832daee41fe1
HermanPlay <hermanplay1110@gmail.com>
2023-08-30 19:25:19 +0200

    chagnes

--- dist/swagger-initializer.js
+++ dist/swagger-initializer.js
@@ -5,6 +5,7 @@ window.onload = function() {
   window.ui = SwaggerUIBundle({
     url: "swagger.yaml",
     dom_id: '#swagger-ui',
+    operationsSorter: "method",
     deepLinking: true,
     presets: [
       SwaggerUIBundle.presets.apis,
